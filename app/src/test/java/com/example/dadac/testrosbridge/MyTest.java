bd5e29d212996d81f1a993e601156f54a25a83ea
lichuan <791180016@qq.com>
2021-11-16 09:18:14 +0800

    1116 提交demo

---
+++ app/src/test/java/com/example/dadac/testrosbridge/MyTest.java
@@ -0,0 +1,9 @@
+package com.example.dadac.testrosbridge;
+
+
+public class MyTest {
+    public static void main(String[] args) {
+        /*Class<?> clazz = RosNote.Actuator.getClazz();
+        System.out.println(clazz);*/
+    }
+}
